89497f698461f0856714e771cad80d05b89fd938
Stephan Krug <stekrug@icloud.com>
2022-09-14 19:14:34 -0300

    Adiciona arquivos que faltavam T2

---
+++ T2/cpu.cc
@@ -0,0 +1,38 @@
+#include "cpu.h"
+#include <iostream>
+
+__BEGIN_API
+
+void CPU::Context::save()
+{
+    // Ver casos de erro (retorno -1)...
+    if (getcontext(&_context) == -1) {
+    	std::cout << "Não foi possível salvar o contexto atual." << std::endl;
+    	abort();
+    }
+}
+
+void CPU::Context::load()
+{
+    if (setcontext(&_context) == -1) {
+    	std::cout << "Não foi possível restaurar o contexto." << std::endl;
+    	abort();
+    }
+}
+
+CPU::Context::~Context()
+{
+    if (_stack) {
+        delete _stack;
+    }
+}
+
+void CPU::switch_context(Context *from, Context *to)
+{
+    if(swapcontext(&from->_context, &to->_context) == -1) {
+    	std::cout << "Não foi possível fazer a troca de contextos." << std::endl;
+    	abort();
+    }
+}
+
+__END_API
---
+++ T2/cpu.h
@@ -0,0 +1,72 @@
+#ifndef cpu_h
+#define cpu_h
+
+#include <ucontext.h>
+#include <iostream>
+#include <errno.h>
+#include "traits.h"
+
+
+__BEGIN_API
+
+class CPU
+{
+    public:
+
+        class Context
+        {
+        private:
+            static const unsigned int STACK_SIZE = Traits<CPU>::STACK_SIZE;
+            void defaultContext()
+            {
+                save();
+                _stack = new char[STACK_SIZE];
+                if (_stack == nullptr) {
+                    std::cout << "Sem espaço para a alocação da pilha." << std::endl;
+                    abort();
+                } else {
+                    _context.uc_link = 0;
+                    _context.uc_stack.ss_sp = _stack;
+                    _context.uc_stack.ss_size = STACK_SIZE;
+                    _context.uc_stack.ss_flags = 0;
+                }
+            }
+        public:
+            Context() 
+            {
+                defaultContext();
+            }
+
+            template<typename ... Tn>
+            Context(void (* func)(Tn ...), Tn ... an)
+            {
+                // errno -> usado para erros
+                defaultContext();
+                errno = 0;
+                makecontext(&_context, (void (*)(void))func, sizeof...(Tn), an...);
+                if(errno != 0) {
+                    std::cout << "Erro apontado por makecontext()." << std::endl;
+                    abort();
+                }
+            }
+
+            ~Context();
+
+            void save();
+            void load();
+
+        private:            
+            char *_stack;
+        public:
+            ucontext_t _context;
+        };
+
+    public:
+
+        static void switch_context(Context *from, Context *to);
+
+};
+
+__END_API
+
+#endif
